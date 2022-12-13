946c701f8a3dd00fa3fa396a114e773779189c32
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2022-12-13 07:18:38 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -27,8 +27,8 @@ host = request.RawPC("host")
 # UMass cluster
 host.component_manager_id = "urn:publicid:IDN+cloudlab.umass.edu+authority+cm"
 # Assign to the node hosting the FPGA.
-#host.component_id = "pc154"
-host.hardware_type = "fpga-alveo-100g"
+host.component_id = "pc163"
+#host.hardware_type = "fpga-alveo-100g"
 # Use the default image for the type of the node selected. 
 host.setUseTypeDefaultImage()
 
@@ -37,7 +37,7 @@ fpga = request.RawPC("fpga")
 # UMass cluster
 fpga.component_manager_id = "urn:publicid:IDN+cloudlab.umass.edu+authority+cm"
 # Assign to the fgpa node
-#fpga.component_id = "fpga-pc16x"
+#fpga.component_id = "fpga-pc163"
 # Use the default image for the type of the node selected. 
 fpga.setUseTypeDefaultImage()
 
